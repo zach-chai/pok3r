7501cfd09e7861e958becbe7bd49087d54f7236b
Zachary Chai <zachary.chai@outlook.com>
2015-09-23 16:50:42 -0400

    test handrank

---
+++ src/test/HandRankTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import main.Card;
+import main.HandCardRank;
+import main.HandRank;
+import main.Suit;
+import main.Value;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HandRankTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompareToRank() throws Exception {
+		
+		ArrayList<HandRank> ranks = new ArrayList<HandRank>();
+		for(HandCardRank rank: HandCardRank.values()) {			
+			ranks.add(new HandRank(rank, new Card()));
+		}
+		
+		for(int i = 0; i < ranks.size() - 1; i++) {
+			assertTrue(ranks.get(i).compareTo(ranks.get(i + 1)) < 1);
+		}
+	}
+	
+	@Test
+	public void testCompareToHighCard() throws Exception {
+		
+		HandCardRank rank = HandCardRank.Straight;
+		HandRank lowerHighCard = new HandRank(rank, new Card(Value.EIGHT, Suit.CLUBS));
+		HandRank higherHighCard = new HandRank(rank, new Card(Value.JACK, Suit.CLUBS));
+		
+		assertTrue(lowerHighCard.compareTo(higherHighCard) < 0);
+	}
+
+}
