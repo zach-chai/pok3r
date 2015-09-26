e63c0adc7d7dec4d3d56aef4b765cbacfc3a0b33
Zachary Chai <zachary.chai@carleton.ca>
2015-09-25 22:49:58 -0400

    tests for constructing cards from strings

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
 import main.Card;
 import main.Suit;
 import main.Value;
@@ -8,6 +12,23 @@ import main.Value;
 import org.junit.Test;
 
 public class CardTest {
+	
+	Card card1;
+	Card card2;
+	Card card3;
+	Card card4;
+	
+	@Before
+	public void setUp() throws Exception {		
+		card1 = new Card(Value.ACE, Suit.SPADES);
+		card2 = new Card(Value.TWO, Suit.HEARTS);
+		card3 = new Card(Value.KING, Suit.DIAMONDS);
+		card4 = new Card(Value.FOUR, Suit.CLUBS);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 	
 	@Test
 	public void testCard() throws Exception {
@@ -24,12 +45,14 @@ public class CardTest {
 	
 	@Test
 	public void testToString() throws Exception {
-		
-		Card card1 = new Card(Value.ACE, Suit.SPADES);
-		Card card2 = new Card(Value.TWO, Suit.HEARTS);
-		
 		assertEquals("AceSpades", card1.toString());
 		assertEquals("TwoHearts", card2.toString());
+	}
+	
+	@Test
+	public void testConstructingFromString() throws Exception {
+		assertEquals(card4, new Card("FourClubs"));
+		assertEquals(card3, new Card("KingDiamonds"));
 	}
 	
 }
