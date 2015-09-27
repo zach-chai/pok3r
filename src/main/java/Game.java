bf60d8062769ce7619628872356c2094d160d4d8
Zachary Chai <zachary.chai@outlook.com>
2015-09-27 17:10:21 -0400

    Fix up entry point class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,15 +4,33 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Game {
-
-	public static void main(String[] args) {
+	
+	Round round;
+	
+	public Game() {
+		round = new Round(new BufferedReader(new InputStreamReader(System.in)),
+						new BufferedWriter(new OutputStreamWriter(System.out)));
+	}
+	
+	public void startGame() {
 		System.out.println("Welcome to Poker Ranking");
-		
-		Round round = new Round(new BufferedReader(new InputStreamReader(System.in)), new BufferedWriter(new OutputStreamWriter(System.out)));
+		do {
+			startRound();
+			System.out.println("Press 0 to play again or 1 to exit");
+		} while (round.getInput().equals("0"));
+		round.cleanup();
+	}
+	
+	private void startRound() {
 		round.setupRound();
 		round.inputPlayerHands();
 		round.rankPlayers();
 		round.outputRanks();
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.startGame();
 	}
 
 }
